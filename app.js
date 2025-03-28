dd391b3a96db51316dfbb0c9e7c20a81199ee2a4
Andrew <andrewjin@outlook.com>
2025-03-28 13:35:52 -0400

    rewrote portfolio html with vue js component to reduce clutter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 Vue.createApp({})
 
-.component('PortfolioEntry', {
+.component('portfolio-entry', {
     template: `
         <div class = "row">
             <div class = "col-lg-10">
@@ -8,7 +8,7 @@ Vue.createApp({})
                 <p>{{ text }}</p>
             </div>
             <div class = "col btncol">
-                <a href = "{{ url }}" style = "margin: auto 0px auto auto;"><button class = "button" tabindex = "-1">View <span class = "caret">&rsaquo;</span></button></a>
+                <a v-bind:href = url style = "margin: auto 0px auto auto;"><button class = "button" tabindex = "-1">View <span class = "caret">&rsaquo;</span></button></a>
             </div>
         </div>
     `,
