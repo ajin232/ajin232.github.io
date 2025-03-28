62fea9c3119442c35fe4c660aeec358eae778f70
Andrew <andrewjin@outlook.com>
2025-03-28 12:00:45 -0400

    test

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const app = Vue.createApp({
-    // Vue app options
-}).mount('#app')
+Vue.createApp({})
+
+.component('PortfolioEntry', {
+    template: `
+        <div class = "row">
+            <div class = "col-lg-10">
+                <h4>{{ name }}</h4>
+                <p>{{ text }}</p>
+            </div>
+            <div class = "col btncol">
+                <a href = "{{ url }}" style = "margin: auto 0px auto auto;"><button class = "button" tabindex = "-1">View <span class = "caret">&rsaquo;</span></button></a>
+            </div>
+        </div>
+    `,
+    props: ['name', 'text', 'url']
+})
+.mount('#app')
